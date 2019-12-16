ccd139e6e93e58b72a1019350169e87ab81e1daf
JoshuaGX <47896703+JoshuaGX@users.noreply.github.com>
2019-12-15 18:06:08 -0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+import { fromEventPattern } from 'rxjs';
+import { ResultsComponent } from './results/results.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionFormComponent,
+    ResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
